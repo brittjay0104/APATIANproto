e42e7c9171d411f19a9d90cb388692fbcbb7232e
Rahul Pandita <rahulpandita@gmail.com>
2015-09-24 16:48:59 -0400

    Refactored Main Method Added Configuration Class

--- src/developer_creator/RunAnalysis.java
+++ src/developer_creator/RunAnalysis.java
@@ -10,277 +10,282 @@ import org.eclipse.jgit.errors.RepositoryNotFoundException;
 
 import code_parser.ModelRepository;
 import code_parser.ModelSourceFile;
+import util.Configuration;
 
 public class RunAnalysis {
 
-		public static void main(String[] args) throws Exception {
-			
-			System.setOut(new PrintStream(new FileOutputStream("console-output.txt")));
-			
-			Runtime rt = Runtime.getRuntime();
-			
-			String repo = "./dummy-repo3/.git";
-			File repoGit = new File(repo);
-			ModelDeveloper dev = new ModelDeveloper("Brittany Johnson");
-			dev.setUserName("brittjay0104");
-			// TODO add pseudo name to reporting?
-			//dev.setPseudoName("Joyce");
-			String repoName = "dummy-repo3";
-			String dir = "./dummy-repo3/";
-			File directory = new File (dir);
-			
-			clearOutDirectory(directory);
-			directory.delete();
-			
-			Process p3 = rt.exec("git clone https://github.com/brittjay0104/dummy-repo3.git");
-			//Process p3 = rt.exec("git clone -b landing-strip https://github.com/ataulm/wutson.git"); 
-			System.out.println(p3.waitFor());
-									
-			//System.out.println("Project cloned!");
-			
-			
-			//set repository history
-			ModelRepository repository = new ModelRepository(repoGit);
-			Git gitHub = repository.getGitRepository();
-			
-			if (repository.setRepositoryRevisionHistory(gitHub, dev) != null){
-				repository.setRepositoryRevisionHistory(gitHub, dev);
-				
-				//ArrayList<RevCommit> commits = repository.getRevisions();
+	public static String repoName = "dummy-repo";
+	public static String userName = "brittjay0104";
+	public static String developerName = "Brittany Johnson";
+	public static String github_url = "https://github.com/" + userName + "/" + repoName + ".git";
+	public static String gitCloneCmd = "git clone " + github_url;
+	public static String localRepoDir = "." + File.separator + repoName + File.separator;
+	public static String repoLocalFile = localRepoDir + ".git";
+	
+	
 
-				//set source files for each directory
-				repository.setSourceFiles(dir);
-				
-				//set history for each file
-				for (ModelSourceFile f: repository.getSourceFiles()) {
-					repository.setFileRevisionHistory(gitHub, f);
-				}
-								
-				//Analyze ASTs for all revisions (right now for null checks)
-				repository.revertAndAnalyzeForNull(gitHub, dir, dev, repoName);
-//				/repository.analyzeForMethodInvocations(gitHub, "", dev, repoName);
-			}	
-			
-//			ModelSourceFile f = new ModelSourceFile(new File ("./dummy-repo/NullObjectPattern_test.java"));
-//			
-//			ModelParser p = new ModelParser();
-//			p.parseForNODP(f);
-//			p.parseForNPEAvoidance(f);
-			
-		}	
-				
-				
+	public static void main(String[] args) throws Exception {
 
-			
-//			File users = new File("test-github-users.txt");
-//			FileReader in1 = new FileReader(users);
-//			BufferedReader br = new BufferedReader(in1);
-//			
-//			String line;
-//			while ((line=br.readLine()) != null){
-//				Runtime rt = Runtime.getRuntime();
-//				String username = line;
-//				//dummyRepoRun(rt);
-//				
-//				// get all repositories for a given Github account/username
-//				//String username = "mpeterson2";
-//				ModelDeveloper dev = new ModelDeveloper();
-//				dev.setUserName(username);
-			// TODO can I just set the username and name here rather than automating it? --> use it to check to make sure commit belongs to dev 
-//				Process p1 = rt.exec("C:/SW/CommandLine/GnuWin32/bin/wget --no-check-certificate -O user.txt https://api.github.com/users/" + username);
-//				// Process p1 = rt.exec("curl -o user.txt https://api.github.com/users/" + username);
-//				System.out.println(p1.waitFor());
-//				
-//				FileReader in2 = new FileReader(new File("user.txt"));
-//				BufferedReader br1 = new BufferedReader(in2);
-//				
-//				String line1;
-//				while ((line1=br1.readLine()) != null){
-//					if (line1.contains("name")){
-//						String developer = line1.substring(line1.indexOf(":") + 3, line1.length()-2);
-//						System.out.println(developer);
-//						dev.setDevName(developer);
-//					} 
-//				}
-//				
-//				if (dev.getDevName() == null){
-//					FileReader in5 = new FileReader(new File("user.txt"));
-//					BufferedReader br5 = new BufferedReader(in5);
-//					
-//					String line5;
-//					while ((line5=br5.readLine()) != null){
-//						if (line5.contains("login")){
-//							String loginName = line5.substring(line5.indexOf(":") + 3, line5.length()-2);
-//							System.out.println(loginName);
-//							dev.setDevName(loginName);								
-//							
-//						}						
-//					}
-//					
-//					br5.close();
-//				}
-//				
-//				br1.close();
-//				
-//				
-//				Process p2 = rt.exec("C:/SW/CommandLine/GnuWin32/bin/wget --no-check-certificate -O output.txt https://api.github.com/search/repositories?q=language:java+user:" + username);
-//				//Process p2 = rt.exec("curl -o output.txt https://api.github.com/search/repositories?q=language:java+user:" + username);
-//				System.out.println(p2.waitFor());
-//				
-//				FileReader in3 = new FileReader(new File("output.txt"));
-//				ArrayList<String> urls = new ArrayList<String>();
-//				BufferedReader br3 = new BufferedReader(in3);
-//				
-//				String line2;
-//				while ((line2 = br3.readLine()) != null){
-//					if (line2.contains("clone_url")){
-//						String url = line2.substring(line2.indexOf("http"), line2.lastIndexOf("\""));
-//						System.out.println(url);
-//						urls.add(url);
-//					}
-//				}
-//				
-//				br3.close();
-//				
-//				
-//				FileReader in4 = new FileReader(new File("output.txt"));
-//				ArrayList<String> repoNames = new ArrayList<String>();
-//				BufferedReader br4 = new BufferedReader(in4);
-//				
-//				String line3;
-//				while ((line3 = br4.readLine()) != null){
-//					if (line3.contains("full_name")){
-//						String repoName = line3.substring(line3.indexOf("/")+1, line3.lastIndexOf("\""));
-//						System.out.println(repoName);
-//						repoNames.add(repoName);
-//					}
-//				}
-//				
-//				
-//				br4.close();
-//				
-//				
-//				String repoName = "";
-//
-//				// clone from list of urls (also gather repoNames for reporting -- should match up with number of urls)
-//				for (int i=0; i < urls.size(); i++){
-//					if (repoNames.get(i) != null){
-//						repoName = repoNames.get(i);						
-//					}
-//					
-//					// for cloning
-//					String cloneURL = urls.get(i);
-//					
-//					
-//					System.out.println(cloneURL);
-//					// for creating repository
-//					String directory = "." + cloneURL.substring(cloneURL.lastIndexOf("/"), cloneURL.length());
-//					System.out.println(directory);
-//					File repoDir = new File(directory);
-//					
-//					clearOutDirectory(repoDir);
-//					repoDir.delete();
-//					
-//					Process p3 = rt.exec("git clone " + cloneURL);
-//					System.out.println(p3.waitFor());
-//					
-//					String repo = directory + "/.git";
-//					File repoGit = new File(repo);
-//					
-//					//set repository history
-//					ModelRepository repository = new ModelRepository(repoGit);
-//					Git gitHub = repository.getGitRepository();
-//					
-//					if (repository.setRepositoryRevisionHistory(gitHub, dev) != null){
-//						repository.setRepositoryRevisionHistory(gitHub, dev);
-//						
-//						//set source files for each directory
-//						repository.setSourceFiles(directory);
-//						
-//						//set history for each file
-//						for (ModelSourceFile f: repository.getSourceFiles()) {
-//							repository.setFileRevisionHistory(gitHub, f);
-//						}
-//						
-//						//Analyze ASTs for all revisions (right now for null checks)
-//						repository.revertAndAnalyzeForNull(gitHub, directory, dev, repoName);
-//						
-//					}
-//				}
-//			}
-//			
-//			br.close();
-		
-		
+		System.setOut(new PrintStream(new FileOutputStream(Configuration.opFile)));
 
+		Runtime rt = Runtime.getRuntime();
 
-		private static void dummyRepoRun(Runtime rt) throws IOException, InterruptedException, RepositoryNotFoundException {
-			String directory = "./dummy-repo2";
-			File repoDir = new File(directory);
+		ModelDeveloper dev = new ModelDeveloper(developerName);
+		dev.setUserName(userName);
+		// TODO add pseudo name to reporting?
+		// dev.setPseudoName("Joyce");
 
-			clearOutDirectory(repoDir);
-			repoDir.delete();
+		File directory = new File(localRepoDir);
 
-			Process p = rt.exec("git clone https://github.com/brittjay0104/dummy-repo2.git");
-			System.out.println(p.waitFor());
+		clearOutDirectory(directory);
+		directory.delete();
 
-			String repository = "./dummy-repo2/.git";
-			File repoGit = new File(repository);
+		Process p3 = rt.exec(gitCloneCmd);
 
-			String developer = "Brittany Johnson";
-			ModelDeveloper dev = new ModelDeveloper(developer);
-			
-			String repoName = "dummy-repo2";
+		System.out.println(p3.waitFor());
 
+		// System.out.println("Project cloned!");
 
-			// Set repository history (all commits for the repository)
-			ModelRepository repo = new ModelRepository(repoGit);
-			Git gitHub = repo.getGitRepository();
+		// set repository history
+		ModelRepository repository = new ModelRepository(new File(repoLocalFile));
+		Git gitHub = repository.getGitRepository();
 
-			repo.setRepositoryRevisionHistory(gitHub, dev);
+		if (repository.setRepositoryRevisionHistory(gitHub, dev) != null) {
+			repository.setRepositoryRevisionHistory(gitHub, dev);
 
+			// ArrayList<RevCommit> commits = repository.getRevisions();
 
-			// Set source files for directory/repository
-			repo.setSourceFiles(directory);
+			// set source files for each directory
+			repository.setSourceFiles(localRepoDir);
 
-			// Set history for each file in directory
-			for (ModelSourceFile f: repo.getSourceFiles()) {
-				repo.setFileRevisionHistory(gitHub, f);
+			// set history for each file
+			for (ModelSourceFile f : repository.getSourceFiles()) {
+				repository.setFileRevisionHistory(gitHub, f);
 			}
 
-
-			//Analyze ASTs for all revisions (right now for null checks)
-			repo.revertAndAnalyzeForNull(gitHub, directory, dev, repoName);
+			// Analyze ASTs for all revisions (right now for null checks)
+			repository.revertAndAnalyzeForNull(gitHub, localRepoDir, dev, repoName);
+			// /repository.analyzeForMethodInvocations(gitHub, "", dev,
+			// repoName);
 		}
 
+		// ModelSourceFile f = new ModelSourceFile(new File
+		// ("./dummy-repo/NullObjectPattern_test.java"));
+		//
+		// ModelParser p = new ModelParser();
+		// p.parseForNODP(f);
+		// p.parseForNPEAvoidance(f);
 
-		public static boolean clearOutDirectory(File rootDirectory)
-		{
-			if (!rootDirectory.exists() || (rootDirectory.isDirectory() && rootDirectory.listFiles().length == 0))
-			{
-				return true;
-			}
-			return recursivelyClearDirectory(rootDirectory);
+	}
+
+	// File users = new File("test-github-users.txt");
+	// FileReader in1 = new FileReader(users);
+	// BufferedReader br = new BufferedReader(in1);
+	//
+	// String line;
+	// while ((line=br.readLine()) != null){
+	// Runtime rt = Runtime.getRuntime();
+	// String username = line;
+	// //dummyRepoRun(rt);
+	//
+	// // get all repositories for a given Github account/username
+	// //String username = "mpeterson2";
+	// ModelDeveloper dev = new ModelDeveloper();
+	// dev.setUserName(username);
+	// TODO can I just set the username and name here rather than automating it?
+	// --> use it to check to make sure commit belongs to dev
+	// Process p1 = rt.exec("C:/SW/CommandLine/GnuWin32/bin/wget
+	// --no-check-certificate -O user.txt https://api.github.com/users/" +
+	// username);
+	// // Process p1 = rt.exec("curl -o user.txt https://api.github.com/users/"
+	// + username);
+	// System.out.println(p1.waitFor());
+	//
+	// FileReader in2 = new FileReader(new File("user.txt"));
+	// BufferedReader br1 = new BufferedReader(in2);
+	//
+	// String line1;
+	// while ((line1=br1.readLine()) != null){
+	// if (line1.contains("name")){
+	// String developer = line1.substring(line1.indexOf(":") + 3,
+	// line1.length()-2);
+	// System.out.println(developer);
+	// dev.setDevName(developer);
+	// }
+	// }
+	//
+	// if (dev.getDevName() == null){
+	// FileReader in5 = new FileReader(new File("user.txt"));
+	// BufferedReader br5 = new BufferedReader(in5);
+	//
+	// String line5;
+	// while ((line5=br5.readLine()) != null){
+	// if (line5.contains("login")){
+	// String loginName = line5.substring(line5.indexOf(":") + 3,
+	// line5.length()-2);
+	// System.out.println(loginName);
+	// dev.setDevName(loginName);
+	//
+	// }
+	// }
+	//
+	// br5.close();
+	// }
+	//
+	// br1.close();
+	//
+	//
+	// Process p2 = rt.exec("C:/SW/CommandLine/GnuWin32/bin/wget
+	// --no-check-certificate -O output.txt
+	// https://api.github.com/search/repositories?q=language:java+user:" +
+	// username);
+	// //Process p2 = rt.exec("curl -o output.txt
+	// https://api.github.com/search/repositories?q=language:java+user:" +
+	// username);
+	// System.out.println(p2.waitFor());
+	//
+	// FileReader in3 = new FileReader(new File("output.txt"));
+	// ArrayList<String> urls = new ArrayList<String>();
+	// BufferedReader br3 = new BufferedReader(in3);
+	//
+	// String line2;
+	// while ((line2 = br3.readLine()) != null){
+	// if (line2.contains("clone_url")){
+	// String url = line2.substring(line2.indexOf("http"),
+	// line2.lastIndexOf("\""));
+	// System.out.println(url);
+	// urls.add(url);
+	// }
+	// }
+	//
+	// br3.close();
+	//
+	//
+	// FileReader in4 = new FileReader(new File("output.txt"));
+	// ArrayList<String> repoNames = new ArrayList<String>();
+	// BufferedReader br4 = new BufferedReader(in4);
+	//
+	// String line3;
+	// while ((line3 = br4.readLine()) != null){
+	// if (line3.contains("full_name")){
+	// String repoName = line3.substring(line3.indexOf("/")+1,
+	// line3.lastIndexOf("\""));
+	// System.out.println(repoName);
+	// repoNames.add(repoName);
+	// }
+	// }
+	//
+	//
+	// br4.close();
+	//
+	//
+	// String repoName = "";
+	//
+	// // clone from list of urls (also gather repoNames for reporting -- should
+	// match up with number of urls)
+	// for (int i=0; i < urls.size(); i++){
+	// if (repoNames.get(i) != null){
+	// repoName = repoNames.get(i);
+	// }
+	//
+	// // for cloning
+	// String cloneURL = urls.get(i);
+	//
+	//
+	// System.out.println(cloneURL);
+	// // for creating repository
+	// String directory = "." + cloneURL.substring(cloneURL.lastIndexOf("/"),
+	// cloneURL.length());
+	// System.out.println(directory);
+	// File repoDir = new File(directory);
+	//
+	// clearOutDirectory(repoDir);
+	// repoDir.delete();
+	//
+	// Process p3 = rt.exec("git clone " + cloneURL);
+	// System.out.println(p3.waitFor());
+	//
+	// String repo = directory + "/.git";
+	// File repoGit = new File(repo);
+	//
+	// //set repository history
+	// ModelRepository repository = new ModelRepository(repoGit);
+	// Git gitHub = repository.getGitRepository();
+	//
+	// if (repository.setRepositoryRevisionHistory(gitHub, dev) != null){
+	// repository.setRepositoryRevisionHistory(gitHub, dev);
+	//
+	// //set source files for each directory
+	// repository.setSourceFiles(directory);
+	//
+	// //set history for each file
+	// for (ModelSourceFile f: repository.getSourceFiles()) {
+	// repository.setFileRevisionHistory(gitHub, f);
+	// }
+	//
+	// //Analyze ASTs for all revisions (right now for null checks)
+	// repository.revertAndAnalyzeForNull(gitHub, directory, dev, repoName);
+	//
+	// }
+	// }
+	// }
+	//
+	// br.close();
+
+	private static void dummyRepoRun(Runtime rt) throws IOException, InterruptedException, RepositoryNotFoundException {
+		String directory = "./dummy-repo2";
+		File repoDir = new File(directory);
+
+		clearOutDirectory(repoDir);
+		repoDir.delete();
+
+		Process p = rt.exec("git clone https://github.com/brittjay0104/dummy-repo2.git");
+		System.out.println(p.waitFor());
+
+		String repository = "./dummy-repo2/.git";
+		File repoGit = new File(repository);
+
+		String developer = "Brittany Johnson";
+		ModelDeveloper dev = new ModelDeveloper(developer);
+
+		String repoName = "dummy-repo2";
+
+		// Set repository history (all commits for the repository)
+		ModelRepository repo = new ModelRepository(repoGit);
+		Git gitHub = repo.getGitRepository();
+
+		repo.setRepositoryRevisionHistory(gitHub, dev);
+
+		// Set source files for directory/repository
+		repo.setSourceFiles(directory);
+
+		// Set history for each file in directory
+		for (ModelSourceFile f : repo.getSourceFiles()) {
+			repo.setFileRevisionHistory(gitHub, f);
 		}
 
-		private static boolean recursivelyClearDirectory(File parentDirectory)
-		{
-			for (File f : parentDirectory.listFiles())
-			{
-				if (f.isDirectory())
-				{
-					if (!recursivelyClearDirectory(f))
-						return false;
-				}
-				if (!f.delete())
-				{
+		// Analyze ASTs for all revisions (right now for null checks)
+		repo.revertAndAnalyzeForNull(gitHub, directory, dev, repoName);
+	}
+
+	public static boolean clearOutDirectory(File rootDirectory) {
+		if (!rootDirectory.exists() || (rootDirectory.isDirectory() && rootDirectory.listFiles().length == 0)) {
+			return true;
+		}
+		return recursivelyClearDirectory(rootDirectory);
+	}
+
+	private static boolean recursivelyClearDirectory(File parentDirectory) {
+		for (File f : parentDirectory.listFiles()) {
+			if (f.isDirectory()) {
+				if (!recursivelyClearDirectory(f))
 					return false;
-				}
 			}
-			return parentDirectory.listFiles().length == 0;
+			if (!f.delete()) {
+				return false;
+			}
 		}
-
-
-		
+		return parentDirectory.listFiles().length == 0;
+	}
 
 }
