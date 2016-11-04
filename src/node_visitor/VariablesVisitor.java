412940a087b9d39197d025b601991192fa5fb0a9
Brittany Johnson <bjohnson2011@gmail.com>
2016-11-04 11:06:48 -0400

    Making sure repo up to date

---
+++ src/node_visitor/VariablesVisitor.java
@@ -0,0 +1,13 @@
+package node_visitor;
+
+import code_parser.ModelSourceFile;
+
+public class VariablesVisitor extends AbstractVisitor{
+
+	public VariablesVisitor(ModelSourceFile file) {
+		super(file);
+	}
+	
+	
+
+}
