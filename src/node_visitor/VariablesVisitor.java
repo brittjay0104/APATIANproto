c543665a43ff0cfc67d3a8fc1a98b6c8ff7b0e53
Rahul Pandita <rpandita@phasechangesoftware.com>
2016-12-02 06:29:34 -0700

    Added place-holders to extract variable and field information

--- src/node_visitor/VariablesVisitor.java
+++ src/node_visitor/VariablesVisitor.java
@@ -1,5 +1,11 @@
 package node_visitor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.FieldDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
+
 import code_parser.ModelSourceFile;
 
 /**
@@ -14,6 +20,18 @@ public class VariablesVisitor extends AbstractVisitor{
 		super(file);
 	}
 	
+	public List<FieldDeclaration> allFieldDeclerations = new ArrayList<>();
+	public List<String> globalVariableList = new ArrayList<>();
 	
+	
+	@Override
+	public boolean visit(FieldDeclaration node) {
+		return super.visit(node);
+	}
+	
+	@Override
+	public boolean visit(VariableDeclarationStatement node) {
+		return super.visit(node);
+	}
 
 }
